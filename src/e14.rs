b4527543f43ad2e75bc335ca5cabc597fb4b62cc
Filip Klimeš <filip@filipklimes.cz>
2023-12-29 12:14:41 +0100

    e14 caching

--- src/e14.rs
+++ src/e14.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 pub(crate) fn e14() {
     println!("{}", longest_collatz(1000000))
 }
@@ -7,9 +5,9 @@ pub(crate) fn e14() {
 fn longest_collatz(arg: usize) -> usize {
     let mut max = 0;
     let mut max_num = 13;
-    let mut cache = HashMap::new();
+    let mut cache = vec!{ 0; 100000 * arg };
     for i in 13..=arg {
-        let len = collatz_len(i, &mut cache);
+        let len = collatz_len_rec(i, &mut cache);
         if len > max {
             max = len;
             max_num = i;
@@ -18,27 +16,26 @@ fn longest_collatz(arg: usize) -> usize {
     max_num
 }
 
-fn collatz_len(mut n: usize, cache: &mut HashMap<u32, u32>) -> usize {
-    let mut len = 0;
-    while n != 1 {
-        if n % 2 == 0 {
-            n /= 2
-        } else {
-            n = 3 * n + 1
-        }
-        len += 1
-    }
-    len + 1 // add 1 for the 1
+fn collatz_len_rec(n: usize, cache: &mut Vec<usize>) -> usize {
+    if n == 1 { return 1; }
+    if cache[n] != 0 { return cache[n] }
+
+    let ans = if n % 2 == 0 {
+        1 + collatz_len_rec(n / 2, cache)
+    } else {
+        1 + collatz_len_rec(3 * n + 1, cache)
+    };
+    cache[n] = ans;
+    ans
 }
 
 #[cfg(test)]
 mod e14_tests {
-    use std::collections::HashMap;
-    use crate::e14::{collatz_len, longest_collatz};
+    use crate::e14::{collatz_len_rec, longest_collatz};
 
     #[test]
     fn e14_works() {
-        assert_eq!(10, collatz_len(13, &mut HashMap::new()));
+        assert_eq!(10, collatz_len_rec(13, &mut vec!( 0; 100 )));
         assert_eq!(837799, longest_collatz(1000000));
     }
 }
